228d9916364ff0f04df7d03f68b4c496abfefeed
adowmandek <adowmandek@gmail.com>
2021-02-21 12:48:42 +0300

    first assignment

---
+++ src/fatuma.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val name:String ="mandek adow"
+    val age=20
+    val phoneNumber="0794764520"
+    var kenyanCitizen:Boolean=true
+    val weight=43
+    println(name)
+    println(age)
+    println(phoneNumber)
+    println(kenyanCitizen)
+}
